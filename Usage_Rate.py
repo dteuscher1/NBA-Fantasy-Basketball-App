ef75390f32ddb2e48c8292cb257e35244c27a1af
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2023-12-19 22:06:01 -0700

    Usage rate by game

--- Usage_Rate.py
+++ Usage_Rate.py
@@ -3,19 +3,24 @@ from nba_api.stats.static import  teams
 import time
 import pandas as pd
 
-nba_teams = teams.get_teams()
-jazz = [x for x in nba_teams if x['full_name'] == 'Utah Jazz'][0]['id']
-jazz
 
-jazz_games = leaguegamefinder.LeagueGameFinder(team_id_nullable=jazz).get_data_frames()[0]['GAME_ID']
+# Get games for the season up to this point
 
-info = boxscoreusagev2.BoxScoreUsageV2(game_id = jazz_games[0])
-all = info.get_data_frames()[0]
-all.info()
-all[['GAME_ID', 'TEAM_ID','TEAM_ABBREVIATION', 'PLAYER_ID', 'PLAYER_NAME', 'USG_PCT']]
-n_games = 120
-
-
+n_games = 333
+season_data = []
+for i in range(1,n_games+1):
+    number_str = str(i)
+    game_num = number_str.zfill(4)
+    game_id_var = f'002230{game_num}'
+    game_info = boxscoreusagev2.BoxScoreUsageV2(game_id = game_id_var).get_data_frames()[0]
+    game_usage = game_info[['GAME_ID', 'TEAM_ID','TEAM_ABBREVIATION', 'PLAYER_ID', 'PLAYER_NAME', 'USG_PCT']]
+    season_data.append(game_usage)
+    if(i % 10 == 0):
+      print(f'Getting game number {i}')
+    
+season_df = pd.concat(season_data)   
+season_df.to_csv('usage_rates.csv')
+    
 yesterdays_games = leaguegamefinder.LeagueGameFinder(league_id_nullable='00', date_to_nullable='12/15/2023', date_from_nullable = '12/15/2023')
 ?leaguegamefinder.LeagueGameFinder()
 games = yesterdays_games.get_data_frames()[0]
@@ -25,12 +30,13 @@ game_ids = games['GAME_ID'].sort_values().unique()
 
 def get_game_ids(today, last_day):
   previous_games = leaguegamefinder.LeagueGameFinder(league_id_nullable = '00', date_to_nullable = today, date_from_nullable = last_day).get_data_frames()[0]
-  game_ids = previous_games[['GAME_ID'].sort_values().unique()
+  game_ids = previous_games['GAME_ID'].sort_values().unique()
   return game_ids
+
 
 test = get_game_ids('12/15/2023', '12/14/2023')
 usage_rate = []
-for game in game_ids:
+for game in test:
   game_info = boxscoreusagev2.BoxScoreUsageV2(game_id = game).get_data_frames()[0]
   game_usage = game_info[['GAME_ID', 'TEAM_ID','TEAM_ABBREVIATION', 'PLAYER_ID', 'PLAYER_NAME', 'USG_PCT']]
   usage_rate.append(game_usage)
