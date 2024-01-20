d69fbece76255685d7e852ccbf5442546486fa36
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2024-01-20 11:20:42 -0700

    Usage rates with game dates

--- Usage_Rate.py
+++ Usage_Rate.py
@@ -2,8 +2,40 @@ from nba_api.stats.endpoints import boxscoretraditionalv2, boxscoreusagev2, leag
 from nba_api.stats.static import  teams
 import time
 import pandas as pd
+import datetime
+pd.options.mode.chained_assignment = None
 
+#initial_day = datetime.date.today()
+initial_day = datetime.date(2023, 11, 15)
+previous_day = initial_day - datetime.timedelta(days=1)
 
+def get_game_ids(today, yesterday):
+  previous_games = leaguegamefinder.LeagueGameFinder(league_id_nullable = '00', date_to_nullable = today.strftime('%m/%d/%Y'), date_from_nullable = yesterday.strftime('%m/%d/%Y')).get_data_frames()[0]
+  game_ids = previous_games['GAME_ID'].sort_values().unique()
+  game_date = previous_games['GAME_DATE'].sort_values().unique()
+  return [game_ids, game_date]
+
+season_data_df = pd.DataFrame()
+while previous_day >= datetime.date(2023, 10, 24):
+  game_ids = get_game_ids(initial_day, previous_day)
+  usage_rate = []
+  for game in game_ids[0]:
+    game_info = boxscoreusagev2.BoxScoreUsageV2(game_id = game).get_data_frames()[0]
+    game_usage = game_info[['GAME_ID', 'TEAM_ID','TEAM_ABBREVIATION', 'PLAYER_ID', 'PLAYER_NAME', 'USG_PCT']]
+    game_usage['GAME_DATE'] = game_ids[1][0]
+    usage_rate.append(game_usage)
+    usage_rate_df = pd.concat(usage_rate)
+  
+  season_data_df = pd.concat([season_data_df, usage_rate_df])  
+  print(f'Getting game data for {previous_day}')
+  initial_day = previous_day
+  previous_day = previous_day - datetime.timedelta(days = 1)
+  
+i = 0
+while i < 5:
+  print(i)
+  i = i + 1
+  
 # Get games for the season up to this point
 
 n_games = 333
@@ -22,8 +54,9 @@ season_df = pd.concat(season_data)
 
 previous_games = pd.read_csv('usage_rates.csv')
 all_season_data = pd.concat([previous_games, season_df])
-all_season_data.to_csv('usage_rates.csv')
+season_data_df.to_csv('usage_rates.csv')
     
+game = leaguegamefinder.LeagueGameFinder(league_id_nullable = '00', game_id_nullable = game_id_var).get_data_frames()[0]
 yesterdays_games = leaguegamefinder.LeagueGameFinder(league_id_nullable='00', date_to_nullable='12/15/2023', date_from_nullable = '12/15/2023')
 ?leaguegamefinder.LeagueGameFinder()
 games = yesterdays_games.get_data_frames()[0]
--- daily_usage.py
+++ daily_usage.py
@@ -10,14 +10,16 @@ yesterdays_date = (datetime.date.today() - datetime.timedelta(days=1)).strftime(
 def get_game_ids(today, yesterday):
   previous_games = leaguegamefinder.LeagueGameFinder(league_id_nullable = '00', date_to_nullable = today, date_from_nullable = yesterday).get_data_frames()[0]
   game_ids = previous_games['GAME_ID'].sort_values().unique()
-  return game_ids
+  game_date = previous_games['GAME_DATE'].sort_values().unique()
+  return [game_ids, game_date]
 
 previous_day_games = get_game_ids(todays_date, yesterdays_date)
 
 usage_rate = []
-for game in previous_day_games:
+for game in previous_day_games[0]:
   game_info = boxscoreusagev2.BoxScoreUsageV2(game_id = game).get_data_frames()[0]
   game_usage = game_info[['GAME_ID', 'TEAM_ID','TEAM_ABBREVIATION', 'PLAYER_ID', 'PLAYER_NAME', 'USG_PCT']]
+  game_usage['GAME_DATE'] = previous_day_games[1][0]
   usage_rate.append(game_usage)
   
 
