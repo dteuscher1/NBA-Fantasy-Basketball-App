b3c8c18c558386f1f0db957590dfdcdb9bab02d8
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2024-01-28 21:13:22 -0700

    Usage rates through 1-27-2024

--- daily_usage.py
+++ daily_usage.py
@@ -3,10 +3,13 @@ from nba_api.stats.static import  teams
 import time
 import pandas as pd
 import datetime
+pd.options.mode.chained_assignment = None
 
 todays_date = datetime.date.today().strftime('%m/%d/%Y')
 yesterdays_date = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%m/%d/%Y')
 
+#todays_date = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%m/%d/%Y')
+#yesterdays_date = (datetime.date.today() - datetime.timedelta(days=2)).strftime('%m/%d/%Y')
 def get_game_ids(today, yesterday):
   previous_games = leaguegamefinder.LeagueGameFinder(league_id_nullable = '00', date_to_nullable = today, date_from_nullable = yesterday).get_data_frames()[0]
   game_ids = previous_games['GAME_ID'].sort_values().unique()
@@ -26,5 +29,5 @@ for game in previous_day_games[0]:
 usage_rate_df = pd.concat(usage_rate)
 
 season_data = pd.read_csv('usage_rates.csv')
-all_season_data = pd.concat([season_data, usage_rate_df])
-all_season_data.to_csv('usage_rates.csv')
+all_season_data = pd.concat([usage_rate_df, season_data])
+all_season_data.to_csv('usage_rates.csv', index = False)
